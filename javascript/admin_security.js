b06578e8abf5e811dfc884ce8f097938c6113c81
mblackbeak <bethmralston@gmail>
2024-03-08 16:33:00 -0800

    onclick function changed from on submit for admin

--- javascript/admin_security.js
+++ javascript/admin_security.js
@@ -1,5 +1,5 @@
 // client side validation for admin page forms
-document.getElementById("search_users_submit").onsubmit = function (event) {
+document.getElementById("search_users_submit").on("click", function (event) {
     event.preventDefault();
     const search = document.getElementById("search_users").value;
     if (search === "" || search === null) {
@@ -7,4 +7,4 @@ document.getElementById("search_users_submit").onsubmit = function (event) {
     } else {
         this.submit();
     }
-}
+});
