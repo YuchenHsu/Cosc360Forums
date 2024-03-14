1ae24aca72d64c860994cd1bc0f6475b8023160d
Jacob Tizel <jacob.tizel@gmail.com>
2024-03-14 01:25:33 -0700

    feat(new_post): DB connection set up

--- html/navbar.php
+++ html/navbar.php
@@ -176,13 +176,13 @@
         </figure>
     </div>    
     <div id="create_post" style="display: none">
-        <form class="create_post form-container" action="new_post.php" method="POST" id="create_post_form">
+        <form class="create_post form-container" action="new_post.php" method="POST" id="create_post_form" enctype="multipart/form-data">
             <fieldset>
                 <legend>Create your post:</legend>
                 <label for="title">Post title: <input required type="text" name="title" id="title"></label>
                 <br>
                 <label for="post_body">Post body:</label>
-                <textarea required rows="10" cols="60" placeholder="Insert your post text here:" id="post_body"></textarea>
+                <textarea required rows="10" cols="60" placeholder="Insert your post text here:" id="post_body" name="post_body"></textarea>
                 <!-- below input accepts png and jpeg files -->
                 <br>
                 <label for="post_image">Insert image here: <input type="file" name="post_image" id="post_image" accept="image/png, image/jpeg"></label>
--- html/new_post.php
+++ html/new_post.php
@@ -1,4 +1,32 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        echo "Hi";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST["title"]) && isset($_POST["post_body"])){
+        $title = $_POST["title"];
+        $post_body = $_POST["post_body"];
+
+        try {
+            $connString = "mysql:host=localhost; dbname=forums";
+            $user = "root";
+            $pass = "";
+
+            $pdo = new PDO($connString, $user, $pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);           
+
+            $pdo->beginTransaction();
+
+
+
+            $pdo->commit();
+        } catch (PDOException $e) {
+            // $pdo->rollback();
+            die( $e->getMessage() );
+        }
+
+        // echo("<h1>" . $title . "</h1>");
+        // echo("<h3>" . $post_body . "</h3>");
+
+        // if (isset($_POST["post_image"]){ }
+
+
     }
+}
