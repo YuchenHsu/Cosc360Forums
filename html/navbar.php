4e52013f4cd499948cbc57e83d3888fe84e23a7b
Jacob Tizel <jacob.tizel@gmail.com>
2024-03-14 00:26:58 -0700

    fix(search): label issue
    Labels weren't made properly, but primarily everything was wrapped in a blank <a> tag

--- html/navbar.php
+++ html/navbar.php
@@ -68,13 +68,13 @@
 
 <!-- Make a invisible Search bar that's toggled by the js -->
 <form id="search-form" class="form-container" style="display: none;">
-    <a>
-        <h3>Search by:</h3>
-        <input id="search" type="radio" name="search" value="user" required>
-        <label for="search"><a>User</a></label>
-        <input id="search" type="radio" name="search" value="post" required>
-        <label for="search"><a>Post</a></label>
-    </a>
+
+    <h3>Search by:</h3>
+    <label for="user_search"><a>User</a></label>
+    <input id="user_search" type="radio" name="search" value="user" required>
+    <label for="post_search"><a>Post</a></label>
+    <input id="post_search" type="radio" name="search" value="post" required>
+
     <input id="search" type="text" name="search" required>
     <button id="search_btn" type="submit">Search</button>
 </form>
