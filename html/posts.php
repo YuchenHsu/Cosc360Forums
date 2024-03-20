9c5ea8d403fc2e0fc80c487bbb7c184a8561a363
YuchenHsu <yuchenhsu0615@gmail.com>
2024-03-20 15:57:53 -0700

    trying to change post to include the post category name

--- html/posts.php
+++ html/posts.php
@@ -1,6 +1,6 @@
 <!-- posts.php -->
 <!-- Posts content will be loaded dynamically -->
-<h1>Welcome to My Blog!</h1>
+<h1>Welcome to Our Forum!</h1>
 <p>Here are some interesting posts:</p>
 <?php
     $connString = "mysql:host=localhost; dbname=forums";
@@ -11,7 +11,7 @@
         $pdo = new PDO($connString, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "SELECT title, content, image, post_id FROM post";
+        $sql = "SELECT title, content, image, post_id, category_id FROM post";
         $statement = $pdo->prepare($sql);
         $statement->execute();
 
@@ -24,6 +24,7 @@
 <h2><a href="view_post.php?post_id=<?=$post_id?>">Post <?=$post_id?>: <?=$title?></a></h2>
 
 <?php
+            echo ''.$category_id.'';
             echo '<p>' . nl2br(htmlspecialchars($row['content'])) . '</p>';
             if (!empty($row['image'])) {
                 echo '<img src="data:image/jpeg;base64,' . base64_encode( $row['image'] ) . '" style = "width: 75%; height: auto;"/>';
