510e6e8bf824ec057e6859eab17884d7314efc95
mblackbeak <bethmralston@gmail>
2024-03-22 17:34:23 -0700

    client side security added to delete

--- javascript/admin.js
+++ javascript/admin.js
@@ -8,6 +8,10 @@ $(document).on('submit', '#delete_posts_form', function(e){
     $('input[type=checkbox]:checked').each(function() {
         selected.push($(this).val());
     });
+    if (selected.length === 0) {
+        alert("Please select at least one post to delete.");
+        return;
+    }
     // Manually serialize form data
     var formData = {
         selected: selected
