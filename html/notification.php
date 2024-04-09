a8257ca797f64365e859816061d7502a9931e021
mblackbeak <bethmralston@gmail>
2024-04-09 11:51:04 -0700

    changed to table form and updated css for table

--- html/notification.php
+++ html/notification.php
@@ -1,6 +1,6 @@
 <!-- Make a invisible notification page that's toggled by the js -->
+<link rel="stylesheet" type="text/css" href="../css/notification.css">
 <div id="notification" class="form-container">
-    <h3>Notifications</h3>
     <div id="notification-content">
         <?php
             include "connect.php";
@@ -16,9 +16,22 @@
                     if (count($notifications) == 0) {
                         echo "<p>No notifications</p>";
                     } else {
+                        echo "<table>";
+                        echo "<caption>Notifications</caption>";
+                        echo "<thead>";
+                        echo "<tr>";
+                        echo "<th>Date</th>";
+                        echo "<th>Notification</th>";
+                        echo "</tr>";
+                        echo "<tbody>";
                         foreach ($notifications as $notification) {
-                            echo "<p>{$notification['content']}</p>";
+                            echo "<tr>";
+                            echo "<td>{$notification['created_at']}</td>";
+                            echo "<td>{$notification['content']}</td>";
+                            echo "</tr>";
                         }
+                        echo "</tbody>";
+                        echo "</table>";
                     }
              
                 } catch (PDOException $e) {
